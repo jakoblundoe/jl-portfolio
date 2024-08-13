fd0f95f7e52f54324fb59b3d26057f0fb84b3e19
jlund <dev@jakoblundoe.com>
2024-08-13 16:02:45 +0200

    Add new dark texture background to about, contact and work page.
    Adjusted styling according to new BG's

---
+++ public/js/bundle.dev.js
@@ -0,0 +1,36 @@
+(() => {
+  // ns-hugo:D:\FileFolder\Repos\jl-portfolio\assets\js\modules\cinemamodule.js
+  function printhelloworld() {
+    console.log("hello world!");
+  }
+  function togglevideo() {
+    document.addEventListener("DOMContentLoaded", (event) => {
+      const button = document.getElementById("showreelbutton");
+      const videocontainer = document.getElementById("videocontainer");
+      const video = document.getElementById("showreelvideo");
+      const resetDelay = 500;
+      if (button && videocontainer) {
+        button.addEventListener("click", () => {
+          if (videocontainer.classList.contains("display_none")) {
+            videocontainer.classList.remove("display_none");
+            videocontainer.classList.add("display_flex");
+            video.play();
+          } else {
+            videocontainer.classList.remove("display_flex");
+            videocontainer.classList.add("display_none");
+            video.pause();
+            setTimeout(() => {
+              video.currentTime = 0;
+            }, resetDelay);
+          }
+        });
+      } else {
+        console.error("Button or videocontainer element not found");
+      }
+    });
+  }
+
+  // <stdin>
+  printhelloworld();
+  togglevideo();
+})();
