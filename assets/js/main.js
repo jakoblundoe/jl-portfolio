506b87c2639db943b2dcade04ad01aa727a0bc18
jlund <dev@jakoblundoe.com>
2024-08-13 16:21:49 +0200

    Adjust js script

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,16 @@ import * as cinemaModule from './modules/cinemamodule.js';
 
 cinemaModule.printhelloworld();
 
-// play showreel functionality
 cinemaModule.togglevideo();
+
+// play showreel functionality
+document.addEventListener("DOMContentLoaded", function() {
+    if (window.location.pathname === "/showreel/")
+    {
+        // cinemaModule.togglevideo();
+        console.log("current page is showreel");
+    }
+    else{
+        console.log("current page is not showreel");
+    }
+});
