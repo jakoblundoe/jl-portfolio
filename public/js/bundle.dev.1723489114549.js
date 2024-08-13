dabe0be1c3f7a9c6d038626e779d009ccdcc655c
jlund <dev@jakoblundoe.com>
2024-08-13 10:08:22 +0200

    Delete statics that is not in use
    Former-commit-id: 16039f12a12cbd32317cc6664bc85f2137549be3

--- public/js/bundle.dev.1723489114549.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  // ns-hugo:D:\FileFolder\Repos\jl-portfolio\assets\js\modules\cinemamodule.js
-  function printhelloworld() {
-    console.log("hello wworld!");
-  }
-
-  // <stdin>
-  printhelloworld();
-})();
