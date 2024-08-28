32794f0bbe19519abf11a216ff1d3e015b4df601
jlund <dev@jakoblundoe.com>
2024-08-28 10:23:42 +0200

    Refactor header to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: ['./layouts/**/*.html', './content/**/*.md'],
   theme: {
@@ -11,7 +14,25 @@ module.exports = {
       'inter': ['Inter', 'sans-serif'],
       'mono': ['Roboto Mono', 'monospace'],
     },
-    extend: {},
+    extend: {
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        md: '1px 2px 8px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 }
