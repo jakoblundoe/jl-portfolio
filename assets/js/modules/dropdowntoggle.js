4aacb1f6f83c4405f08207a4883fb23995da0c2e
jlund <dev@jakoblundoe.com>
2024-09-14 13:52:17 +0200

    Fix loop to contain correct content files

--- assets/js/modules/dropdowntoggle.js
+++ assets/js/modules/dropdowntoggle.js
@@ -19,6 +19,18 @@ export function openEducation (dropdownIsOpen) {
     }
 }
 
+export function closeEducation (dropdownIsOpen) {
+    const dropdownContainer = document.getElementById("educationcontent");
+    const dropdownButton = document.getElementById("educationbutton");
+
+    if (dropdownContainer && dropdownButton && dropdownIsOpen) {
+        dropdownButton.classList.add("rotate-180");
+        dropdownContainer.classList.remove("animate-expand");
+        dropdownContainer.classList.add("animate-collapse");
+        return false;
+    }
+}
+
 function addAnimationEventListeners () {
     const animated = document.querySelector(".animate-expand");
     let activeAnimationFrameID;
@@ -41,43 +53,4 @@ function addAnimationEventListeners () {
     });
     
     animationListenersAdded = true;
-}
-
-
-export function closeEducation (dropdownIsOpen) {
-    const dropdownContainer = document.getElementById("educationcontent");
-    const dropdownButton = document.getElementById("educationbutton");
-
-    if (dropdownContainer && dropdownButton && dropdownIsOpen) {
-        dropdownButton.classList.add("rotate-180");
-        dropdownContainer.classList.remove("animate-expand");
-        dropdownContainer.classList.add("animate-collapse");
-        return false;
-    }
-}
-
-// OPEN EXPERIENCE
-export function openExperience (dropdownIsOpen) {
-    const dropdownContainer = document.getElementById("experiencecontent");
-    const dropdownButton = document.getElementById("experiencebutton");
-
-    if (dropdownContainer && dropdownButton && !dropdownIsOpen) {
-        dropdownButton.classList.remove("rotate-180");
-        dropdownContainer.classList.remove("animate-collapse");
-        dropdownContainer.classList.add("animate-expand");
-        return true;
-    }
-
-}
-
-export function closeExperience (dropdownIsOpen) {
-    const dropdownContainer = document.getElementById("experiencecontent");
-    const dropdownButton = document.getElementById("experiencebutton");
-
-    if (dropdownContainer && dropdownButton && dropdownIsOpen) {
-        dropdownButton.classList.add("rotate-180");
-        dropdownContainer.classList.remove("animate-expand");
-        dropdownContainer.classList.add("animate-collapse");
-        return false;
-    }
 }
